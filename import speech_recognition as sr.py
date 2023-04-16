07360515f4a497fdd49329188ef79136d60b847c
codemaster1104 <115527374+codemaster1104@users.noreply.github.com>
2023-04-17 00:21:15 +0530

    Update import speech_recognition as sr.py

--- import speech_recognition as sr.py
+++ import speech_recognition as sr.py
@@ -1,16 +1,20 @@
 import speech_recognition as sr
+import webbrowser as web
 
 
+path="C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
-    #r.adjust_for_ambient_noise(source,duration=0.5)
+    r.adjust_for_ambient_noise(source,duration=0.5)
     audio = r.listen(source)
 
 try:
    
+    madhurvachan = r.recognize_google(audio)
     #print(r.recognize_google(audio, show_all=True))
-    print(r.recognize_google(audio))
+    print(madhurvachan)
+    web.get(path).open(madhurvachan)
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
